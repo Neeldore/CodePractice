e2f3d0a94168c6b7662e9236ef3102298d484c3b
neeldore <neeljain14@gmail.com>
2020-05-13 17:57:19 +0530

    broilerplate code push

---
+++ GFG/src/GFG.java
@@ -0,0 +1,25 @@
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+
+    class GFG {
+        public static void main(String[] args) throws NumberFormatException, IOException {
+            // code
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int numberOfTestCases = Integer.parseInt(br.readLine());
+            for (int testCaseCounter = 0; testCaseCounter < numberOfTestCases; testCaseCounter++) {
+                String testString = br.readLine();
+                System.out.println(testString);
+            }
+        }
+        
+        public static int[] getInteger(String str, int n) {
+            String[] strList = str.split("\\s+");
+            int[] arrayOfNumber = new int[n];
+            for (int i = 0; i < n; i++) {
+                arrayOfNumber[i] = Integer.parseInt(strList[i]);
+            }
+            return arrayOfNumber;
+        }
+
+    }
