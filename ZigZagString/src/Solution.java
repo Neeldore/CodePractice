3ec498abcfbb0d7819a6ac1e69d9f822320dd5b8
Neelkant Jain <neelkant.jain@bridgesgi.com>
2020-07-23 19:50:24 +0530

    other problems

---
+++ ZigZagString/src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void main(String args[]) {
+        System.out.println(convert("PAYPALISHIRING", 3));
+
+    }
+
+    public static String convert(String s, int numRows) {
+        int len = s.length();
+        char oArray[] = s.toCharArray();
+        return "";
+    }
+
+}
